196bf7c5ca9ecda05d3bc4b1f4eef20fdab9b517
Alehandro Persaud <andrew041994@gmail.com>
2020-02-08 20:15:30 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -5,6 +5,10 @@ attr_accessor :first_name, :last_name
       @first_name = first_name
       @last_name = last_name
       @knowledge = []
+    end
+
+    def learn(knowledge)
+      @@knowledge << knowledge
 
     end
 
